03aad14f99d98b75e5684f911a63ace0fa792018
erasmorojastech <erasmorojastech@gmail.com>
2023-11-09 12:00:00 -0300

    Add App component and styles to the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ProfilePicture from './components/ProfilePicture'
+
+function App() {
+  return (
+    <>
+      <ProfilePicture />
+    </>
+  )
+}
+
+export default App
